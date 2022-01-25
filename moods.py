24b55fa396a18860133826ea4303711ab03b63ab
jwc22 <jwc22@kent.ac.uk>
2022-01-25 23:36:34 +0000

    Wills Suggestions
    - import Numpy to find max value in Array
    -match case instead of else if statement

--- moods.py
+++ moods.py
@@ -2,15 +2,16 @@ import spotipy
 import spotipy.util as util
 import json
 import requests
+import numpy as np
 
 # lifx credentials 
-token2 = "lifx token"
+lifx_token = "lifx token"
 headers = {
-    "Authorization": "Bearer %s" % token2,
+    "Authorization": "Bearer %s" % lifx_token,
 }
 
 # spotify credentials
-username='spotify username'
+username ='spotify username'
 scope = 'choose a scope'
 
 # open link to get user access
@@ -22,12 +23,12 @@ spotify = spotipy.Spotify(auth=token)
 # access the current track and find its ID
 current_track = spotify.current_user_playing_track()
 track = current_track['item']
-tId = track['id']
+track_id = track['id']
 
 # use track ID to find track features and add values to a list 
-track_details = spotify.audio_features(tId)
+track_details = spotify.audio_features(track_id)
 track_details_dict = track_details[0]
-check_list = [track_details_dict['danceability'], 
+value_list = [track_details_dict['danceability'], 
     track_details_dict['energy'], 
     track_details_dict['speechiness'], 
     track_details_dict['acousticness'], 
@@ -35,33 +36,37 @@ check_list = [track_details_dict['danceability'],
     track_details_dict['liveness']]
 
 # find maximum value in list, map index to track features
-max = 0
-for i in check_list:
-    if i > max:
-        max = i
-value = check_list.index(max)
+check_array = np.asarray(value_list)
+max_value = np.argmax(check_array)
 
-if value == 0:
-    colour = "red"
-elif value == 1:
-    colour = "blue"
-elif value == 2:
-    colour = "green"
-elif value ==3:
-    colour = "yellow"
-elif value == 4:
-    colour = "white"
-elif value == 5:
-    colour ="pink"
-else:
+try: 
+    # choose colour based on value from list 
+    match max_value:
+        case 0:
+            colour = "red"
+        case 1:
+            colour = "blue"
+        case 2:
+            colour = "green"
+        case 3:
+            colour = "yellow"
+        case 4:
+            colour = "pink"
+        case 5:
+            colour = "purple"
+    
+except:
     print('something went wrong')
 
+# add colour to payoad of request to smartlamp
 payload = {
         "color": colour,
         "power": "on"
     }
 
+# request to smartlamp 
 response =requests.put('https://api.lifx.com/v1/lights/d073d562fa17/state',
                     data=json.dumps(payload), headers=headers)
-            
+
+# print response from smartlamp   
 print(response.text)
