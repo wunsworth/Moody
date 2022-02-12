5b11421b04a54e778f4d5df3ea9b427b5626be78
Will Unsworth <willunsworth16@gmail.com>
2022-02-12 09:05:47 +0000

    .

--- moods.py
+++ moods.py
@@ -11,13 +11,13 @@ headers = {
 }
 
 # spotify credentials
-username ='spotify username'
-scope = 'choose a scope'
+username ='will_u'
+scope = 'user-read-currently-playing'
 
 # open link to get user access
 token = util.prompt_for_user_token(username,scope,client_id='',client_secret='',redirect_uri='')
 
-# generate auth token 
+# generate auth token
 spotify = spotipy.Spotify(auth=token)
 
 # access the current track and find its ID
@@ -28,21 +28,12 @@ track_id = track['id']
 # use track ID to find track features and add values to a list 
 track_details = spotify.audio_features(track_id)
 track_details_dict = track_details[0]
-value_list = [ 
-    track_details_dict['energy'], 
-    track_details_dict['speechiness'], 
-    track_details_dict['instrumentalness']]
+value_list = [ track_details_dict[attribute] for attribute in ['energy', 'speechiness', 'instrumentalness']]
 
 # convert track values into rgb values
-try: 
-    r = str(int(np.floor(256 * value_list[0])))
-    g = str(int(np.floor(256 * value_list[1])))
-    b = str(int(np.floor(256 * value_list[2])))
-    rgb = "rgb:"+r+","+g+","+b
-    print(rgb)
-# print error message when list is empty 
-except:
-    print('something went wrong, track values are most likeley not being received from spotify API')
+assert len(value_list) == 3, "value list of wrong length"
+r, g, b = [np.floor(256 * value) for value in value_list]
+print(f"rgb:{r},{g},{b}")
 
 # add rgb value to payload
 # payload = {
